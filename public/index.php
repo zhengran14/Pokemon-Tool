ea0de3b6fe88e6e253a7dfa3e125a2e4f1a32834
rabbit <rabbit.zheng@qiffa.com>
2023-07-25 17:04:55 +0800

    Add twig

--- public/index.php
+++ public/index.php
@@ -2,16 +2,28 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
+
 $app->addErrorMiddleware(true, false, false);
 // $app->addErrorMiddleware(false, true, true);
+$twig = Twig::create('../view', ['cache' => false]);
+$app->add(TwigMiddleware::create($app, $twig));
 
 $app->get('/', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
     return $response;
 });
+
+$app->get('/hello/{name}', function ($request, $response, $args) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'hello.html', [
+        'name' => $args['name']
+    ]);
+})->setName('profile');
 
 $app->run();
