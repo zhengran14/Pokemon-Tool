c5d3a7a102b7978d25fdb3c623be5cb50e15eebb
rabbit <rabbit.zheng@qiffa.com>
2023-07-28 11:15:33 +0800

    Develop pokedex page

--- public/static/js/pokedex.js
+++ public/static/js/pokedex.js
@@ -1,24 +1,44 @@
+const imgUrl = '/static/res/pm_img/';
+var currentPage = 0, totalPages = 1;
+
 $(document).ready(function () {
-    getPokemons(1);
+    getPokemons();
 });
 
-function getPokemons(page) {
+function autoNextPage() {
+    let needNextPage = $(document).height() - $(window).height() - $(window).scrollTop() < $('.pm:nth-child(1)').height();
+    if (needNextPage) {
+        getPokemons();
+    }
+};
+
+function getPokemons() {
+    if (currentPage + 1 > totalPages) {
+        return;
+    }
+    currentPage++;
+    if ($('#initPageLoading').length <= 0) {
+        $('#pageLoading').removeClass('d-none');
+    }
+    $(window).unbind('resize', autoNextPage);
+    $(window).unbind('scroll', autoNextPage);
     $.ajax({
         type: 'GET',
-        url: `/pokedex/list?page=${page}`,
+        url: `/pokedex/list?page=${currentPage}`,
         contentType: 'application/json;charset=UTF-8',
         async: true,
         success: function (data) {
             result = JSON.parse(data);
             console.log(result);
+            totalPages = result.totalPages;
             for (const pokemon of result.pokemons) {
-                let div = $(`<div class="col col-6 col-sm-4 col-md-3 col-xxl-2">
+                let div = $(`<div id="pm${pokemon.id}" class="pm col col-6 col-sm-4 col-md-3 col-xxl-2">
                                 <div class="card h-100">
                                 <picture>
-                                    <source media="(min-width: 992px)" srcset="/static/res/pm_img/315/${pokemon.img_file}">
-                                    <source media="(min-width: 576px)" srcset="/static/res/pm_img/210/${pokemon.img_file}">
-                                    <source srcset="/static/res/pm_img/210/${pokemon.img_file}">
-                                    <img decoding="async" src="/static/res/pm_img/315/${pokemon.img_file}" class="card-img-top" alt="${pokemon.name_zh_cn}">
+                                    <source media="(min-width: 992px)" srcset="${imgUrl}315/${pokemon.img_file}">
+                                    <source media="(min-width: 576px)" srcset="${imgUrl}210/${pokemon.img_file}">
+                                    <source srcset="${imgUrl}210/${pokemon.img_file}">
+                                    <img decoding="async" src="${imgUrl}315/${pokemon.img_file}" class="card-img-top" alt="${pokemon.name_zh_cn}">
                                 </picture>
                                 <div class="card-body border-top">
                                     <div class="row text-secondary fs-5 ms-1">${pokemon.zukan_id.toString().padStart(4, '0')}</div>
@@ -39,6 +59,13 @@ function getPokemons(page) {
                 div.find('.card-body').after(pokemonTypes);
             }
         },
-        complete: function () {}
+        complete: function () {
+            if ($('#initPageLoading')) {
+                $('#initPageLoading').remove();
+            }
+            $('#pageLoading').addClass('d-none');
+            $(window).bind('resize', autoNextPage);
+            $(window).bind('scroll', autoNextPage);
+        }
     });
 }
